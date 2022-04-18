06443ec66906813357030c5a55bcb62cb80c4505
michaelchankh01 <101359830+michaelchankh01@users.noreply.github.com>
2022-04-18 08:11:00 +0800

    Big Big Update

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
 function outputUpdate(size) {
     document.querySelector('#size').value = size;
 
@@ -32,7 +48,7 @@ function outputUpdate(size) {
                 j.style.fontSize =  "1.5vh";  
                 break;
             case "2":
-                j.style.fontSize =  "2vh";    
+                j.style.fontSize =  "1.9vh";    
                 break;
             case "3":
                 j.style.fontSize =  "2.5vh";
@@ -69,21 +85,117 @@ function outputUpdate(size) {
     }
 };
 
+const wordsToHide = document.querySelectorAll(".wordsToHide");
+const hideWords = document.querySelector("#hideWords");
+const imagesToHide = document.querySelectorAll(".imagesToHide");
+const hideImages = document.querySelector("#hideImages");
+
+if(getCookie("hideWords")){
+    wordsToHide.forEach(i => {
+        i.style="display: none"
+        })
+        if(hideWords!== null){
+            hideWords.checked=true;
+        }
+}
+else{
+    wordsToHide.forEach(i => {
+        i.style="display: "
+    })
+}
+
+if(hideWords!== null){
+    hideWords.addEventListener('change', hideWord);
+}
+function hideWord(){
+    if(hideWords.checked){
+        document.cookie="hideWords=true";
+    }
+    else{
+        document.cookie="hideWords=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+    }
+}
+
+if(getCookie("hideImages")){
+    imagesToHide.forEach(i => {
+        i.style="display: none"
+        })
+        if(hideImages!== null){
+            hideImages.checked=true;
+        }
+}
+else{
+    imagesToHide.forEach(i => {
+        i.style="display: "
+    })
+}
+
+if(hideImages!== null){
+    hideImages.addEventListener('change', hideImage);
+}
+function hideImage(){
+    if(hideImages.checked){
+        document.cookie="hideImages=true";
+    }
+    else{
+        document.cookie="hideImages=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+    }
+}
+
+
+
 const dark = document.querySelector("#dark");
 const contrast = document.querySelector("#contrast");
 
-dark.addEventListener('click', darkMode);
-contrast.addEventListener('click', highContrast);
+if(getCookie("contrast")){
+    document.body.classList.toggle("highContrast");
+    if(contrast!== null){
+        contrast.checked=true;
+    }
+};
+
+if(getCookie("darkmode")){
+    console.log("TEST");
+    document.body.classList.toggle("darkTheme");
+    if(dark!== null){
+        dark.checked=true;
+    }
+};
+
+if(dark!== null){
+    dark.addEventListener('change', darkMode);
+}
+if(contrast!== null){
+    contrast.addEventListener('change', highContrast);
+}
 
 function darkMode(){
     if (contrast.checked){
-        return
+        contrast.checked=false;
+        document.body.classList.toggle("highContrast");
+        document.cookie="contrast=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
     }
-    else{
-    document.body.classList.toggle("darkTheme")
+    
+    document.body.classList.toggle("darkTheme");
+    document.cookie="darkmode=true";
+
+    if (!dark.checked){
+        document.cookie="darkmode=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+    }
+}
+function highContrast(){
+    if (dark.checked){
+        dark.checked=false;
+        document.body.classList.toggle("darkTheme");
+        document.cookie="dark=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+    }
+    
+    document.body.classList.toggle("highContrast");
+    document.cookie="contrast=true";
+
+    if (!contrast.checked){
+        document.cookie="contrast=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
     }
 }
 
-function highContrast(){
-    document.body.classList.toggle("highContrast");
-}
+
